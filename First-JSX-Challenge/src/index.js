ebea2966bc431fb368014b7b0cf3f98956556b81
Akemy Mory <110566982+akemymory@users.noreply.github.com>
2023-09-10 11:06:45 +0000

    Update 17 files

---
+++ First-JSX-Challenge/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorite books</h1>
+    <ul>
+      <li>Harry Potter</li>
+      <li>Lord of the Rings</li>
+      <li>Davinci Code</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
